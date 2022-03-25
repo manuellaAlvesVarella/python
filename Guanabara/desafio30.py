d6af9a60805ba6f29ec813aceb96e66c4d8f0145
manuellaAlvesVarella <100876996+manuellaAlvesVarella@users.noreply.github.com>
2022-03-25 17:42:08 -0300

    Add files via upload

---
+++ Guanabara/desafio30.py
@@ -0,0 +1,6 @@
+número  = int(input('Me diga qualquer número:'))
+resultado = número % 2
+if resultado == 0:
+    print('O número {} é Par'.format(número))
+else :
+    print ('O número {} é ímpar'.format(número))    
