d3f7fa262cd1309addff4ca1c59f3846932d3180
manuellaAlvesVarella <100876996+manuellaAlvesVarella@users.noreply.github.com>
2022-04-01 22:38:14 -0300

    Add files via upload

---
+++ Guanabara/desafio43.py
@@ -0,0 +1,14 @@
+peso = float(input('Qual é o seu peso ? (kg)'))
+altura = float(input('Qual é a sua altura ? (m)'))
+imc = peso / (altura ** 2)
+print ('O IMC dessa pessoa é de {:.1f}'.format(imc))
+if imc < 18.5:
+    print ('Você esta ABAIXO DO PESO normal')
+elif 18.5 <= imc < 25:
+    print ('PARABÉNS, voc esta na faixa de PESO NORMAL ')
+elif 25 <= imc < 30:
+    print ('Você está em SOBREPESO')
+elif 30 <= imc < 40:
+    print ('Você está em OBESIDADE!')
+elif imc >= 40:
+    print ('Você está com OBESIDADE MÓRBINA, cuidado')
