98d0cc6eb709b5dc095fb556407f8a390d656c7c
manuellaAlvesVarella <100876996+manuellaAlvesVarella@users.noreply.github.com>
2022-08-18 15:35:18 -0300

    Add files via upload

---
+++ Guanabara/aula15.py
@@ -0,0 +1,16 @@
+'''n = cont = 0
+while cont < 5:
+    n = int(input('Digite um número: '))
+    cont += 1 '''
+
+'''n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n  
+print ('A soma vale {}'.format(s))'''
+
+'''nome = jonh
+idade = 33
+print (f'O {nome} tem {idade} anos ') '''
